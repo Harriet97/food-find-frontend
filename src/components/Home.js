bd542b88a2d5266b086b0233d8339f8889563a86
Harriet Bicknell <harrietindubai@gmail.com>
2020-03-26 17:17:59 +0000

    restaurantList

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,17 +14,14 @@ class Home extends Component {
   render() {
     return (
       <div style={{ padding: "70px" }}>
-
         <Search onChange={this.onChange} />
+
         <RestaurantList
           restaurants={this.props.restaurants}
           addFav={this.props.addFav}
           loggedIn={this.props.loggedIn}
           favRestaurants={this.props.favRestaurants}
         />
-
-        <RestaurantList restaurants={this.props.restaurants} addFav={this.props.addFav} loggedIn={this.props.loggedIn} favRestaurants={this.props.favRestaurants} />
-
       </div>
     );
   }
